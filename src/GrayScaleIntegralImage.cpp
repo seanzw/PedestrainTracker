306f9c18a4d77a833427d96f6e6c8c6b0a723da6
xianyu Meng <18201162149@163.com>
2015-05-08 21:44:12 +0800

    Revert "rotate matrix"
    This reverts commit 6f60963c1f85152fac3891e59ec0c9e0c8386e16.

--- src/GrayScaleIntegralImage.cpp
+++ src/GrayScaleIntegralImage.cpp
@@ -3,7 +3,6 @@
 GrayScaleIntegralImage::GrayScaleIntegralImage(const cv::Mat &img) : IntegralImage(img) {
 	// Initialize the integral image.
 	intImage = new unsigned int[width * height];
-	tiltIntImage = new unsigned int[(width + 1) * (height + 2)];
 }
 
 GrayScaleIntegralImage::GrayScaleIntegralImage(int w, int h) : IntegralImage(w, h) {
@@ -13,7 +12,6 @@ GrayScaleIntegralImage::GrayScaleIntegralImage(int w, int h) : IntegralImage(w, 
 GrayScaleIntegralImage::~GrayScaleIntegralImage() {
 	if (intImage != NULL) {
 		delete[] intImage;
-		delete[] tiltIntImage;
 	}
 }
 
@@ -32,9 +30,7 @@ void GrayScaleIntegralImage::CalculateInt(const cv::Mat &img) {
 	// Clear the integral image.
 	memset((void *)intImage, 0, sizeof(unsigned int) * width * height);
 
-	// step1(int i) = step[i] / channelBytes;
-	// step1(int i = 0) = step[0] / channelBytes;
-	int step = img.step[0];
+	int step = img.step1();
 
 	// The temp sum for each row.
 	unsigned int sumRow;
@@ -68,31 +64,43 @@ void GrayScaleIntegralImage::CalculateInt(const cv::Mat &img) {
 }
 
 void GrayScaleIntegralImage::CalculateTiltInt(const cv::Mat &img){
-
-	img.step[0];
-
-
 	//clear the integral image
-	memset((unsigned int *)tiltIntImage, 0, sizeof(unsigned int) * (width+1)  * (height+2));
+	memset((unsigned int *)intImage, 0, sizeof(unsigned int) * (width+1)  * (height+2));
 	
 	//size_t step = 1;
 	//calculate the numbers of bytes each channel occupies 
-	int step = img.step[0];
+	int step = img.step1();
+
+	//initialize the boundary
+	for (int col = 0; col != (width + 1); ++col){
+		//RSAT(x,-2) = 0
+		intImage[col] = 0;
+		//RSAT(x,-1) = 0
+		intImage[(width + 1) + col] = 0;
+	}
+
+	
+	for (size_t row = 0; row != (height + 2); ++row){
+		//RSAT(-1,y) = 0
+		intImage[(width + 1)*row] = 0;
+	}
 
 	for (size_t row = 2; row!= (height+2); ++row){
 
 		int curIntPos = row*(width+1);
-		int curImgPos = (row - 2)*step;
+		int curImgPos = row*step;
 
 		for (size_t col = 1; col != (width+1); ++col){
 
 			intImage[curIntPos + col] = intImage[curIntPos - (width + 1) + (col - 1)] +
 				intImage[curIntPos - (width + 1) + (col + 1)] -
 				intImage[curIntPos - 2 * (width + 1) + col] +
-				img.data[curImgPos + col - 1] +
-				img.data[curImgPos - (width + 1) + col - 1];
+				img.data[curImgPos + col] +
+				img.data[curImgPos - (width + 1) + col];
 		}
 
 	}
+
+	
 
 }
